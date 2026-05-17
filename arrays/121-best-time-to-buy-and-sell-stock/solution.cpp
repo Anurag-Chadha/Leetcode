3b4f0e2526fdfa988a70abb7d3a99a2fbfcd16e7
Anurag-Chadha <anuragchadha526@gmail.com>
2026-05-17 12:12:56 +0530

    Implement maxProfit function in Solution class

--- arrays/121-best-time-to-buy-and-sell-stock/solution.cpp
+++ arrays/121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+int maxProfit(vector<int>& prices) {
+    int minVal=prices[0];
+    int profit=0;
+    for(int i=1;i<prices.size();i++){
+        int cost = prices[i]-minVal;
+        profit= max(cost,profit);
+        minVal=min(minVal,prices[i]);
+        
+    }
+    return profit;
+}
+};
