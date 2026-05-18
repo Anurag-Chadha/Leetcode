d5c7b85980376bc296073b38dd1adbe0b689bac4
Anurag-Chadha <anuragchadha526@gmail.com>
2026-05-18 16:50:47 +0530

    Update solution.cpp

--- arrays/2149-rearrange-array-elements-by-sign/solution.cpp
+++ arrays/2149-rearrange-array-elements-by-sign/solution.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector <int> ans(nums.size(),0);
+        int posWrite=0,negWrite=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans[posWrite]=nums[i];
+                posWrite+=2;
+            }else{
+                ans[negWrite]=nums[i];
+                negWrite+=2;
+            }
 
+        }
+        return ans;
+    }
+};
