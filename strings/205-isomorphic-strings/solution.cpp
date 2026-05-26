1cd95d2cbc94069d7ae60b4545de6959479097db
Anurag-Chadha <anuragchadha526@gmail.com>
2026-05-26 18:14:06 +0530

    Update solution.cpp

--- strings/205-isomorphic-strings/solution.cpp
+++ strings/205-isomorphic-strings/solution.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()) return false;
+        unordered_map<char,char> mpp1,mpp2;
+
+        for(int i=0;i<s.length();i++){
+            if((mpp1.find(s[i])!=mpp1.end()&& mpp1[s[i]]!=t[i]) || (mpp2.find(t[i])!=mpp2.end()&& 
+            mpp2[t[i]]!=s[i]))
+            {
+                return false;
+            }else{
+                mpp1[s[i]]=t[i];
+                mpp2[t[i]]=s[i];
+            }
+        }
+        return true;
+    }
+};
 
