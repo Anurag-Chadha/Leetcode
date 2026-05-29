da327fc6b0acf16d41983824d0b1379e4d6c1069
Anurag-Chadha <anuragchadha526@gmail.com>
2026-05-29 20:13:09 +0530

    Update solution.cpp

--- strings/14-longest-common-prefix/solution.cpp
+++ strings/14-longest-common-prefix/solution.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        int n=strs.size();
+        string ans="";
+        for(int i=0;i<min(strs[0].length(),strs[n-1].length());i++){
+            if(strs[0][i]==strs[n-1][i]){
+                ans+=strs[0][i];
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
